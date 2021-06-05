262b46077c1765933842e31b8aaf332715d3ebf1
Driss BIAZ <drissbiaz.dev@gmail.com>
2021-06-05 17:19:32 +0200

    chore: initial commit

---
+++ src/main/java/fr/dbiaz/cmpmanager/CmpManagerApplication.java
@@ -0,0 +1,13 @@
+package fr.dbiaz.cmpmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmpManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmpManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/dbiaz/cmpmanager/CmpManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dbiaz.cmpmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmpManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
